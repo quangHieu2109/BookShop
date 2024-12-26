4adc7a5800e4539258103b21786d230b1db8d7ee
hai000 <21130343@st.hcmuaf.edu.vn>
2024-12-26 19:00:22 +0700

    update

--- BookshopAPI2/BookshopAPI/obj/Debug/net8.0/BookshopAPI.AssemblyInfo.cs
+++ BookshopAPI2/BookshopAPI/obj/Debug/net8.0/BookshopAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookshopAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230f4d3ad6c14d7082f1d579beee206333160e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe3c8bc83fc58723becd1fabcc5cbec8d527e205")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookshopAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookshopAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
